8b9b71c8a35218a0ec1201ec370462890aa87db5
l-huisman <huisman.l@yahoo.com>
2022-09-21 10:48:39 +0200

    Kut mongo

--- DAL/src/main/java/com/ProjectTicketSystem/BaseDAO.java
+++ DAL/src/main/java/com/ProjectTicketSystem/BaseDAO.java
@@ -3,4 +3,6 @@ package com.ProjectTicketSystem;
 public class BaseDAO
 {
     public static final String DB_URI = "mongodb+srv://user:user@users.jbnxxyk.mongodb.net/test ";
+
+    MongoClientURI uri = new MongoClientURI(DB_URI);
 }
