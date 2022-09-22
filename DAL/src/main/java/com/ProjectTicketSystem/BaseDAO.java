fc1f2eb70eb748bd4ee7592206ca7fe35ce6bdbe
TimV22 <Timv22.tim@gmail.com>
2022-09-22 14:54:14 +0200

    BaseDao

--- DAL/src/main/java/com/ProjectTicketSystem/BaseDAO.java
+++
@@ -1,8 +0,0 @@
-package com.ProjectTicketSystem;
-
-public class BaseDAO
-{
-    public static final String DB_URI = "mongodb+srv://user:user@users.jbnxxyk.mongodb.net/test ";
-
-    MongoClientURI uri = new MongoClientURI(DB_URI);
-}
---
+++ DAL/src/main/java/com/ProjectTicketSystemDal/BaseDAO.java
@@ -0,0 +1,32 @@
+package com.ProjectTicketSystemDal;
+
+
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+
+
+public class BaseDAO
+{
+    private final String uri = "mongodb+srv://user:user@users.jbnxxyk.mongodb.net/test ";
+
+
+    protected void ConnectDatabase() {
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("admin");
+            try {
+                Bson command = new BsonDocument("ping", new BsonInt64(1));
+                Document commandResult = database.runCommand(command);
+                System.out.println("Connected successfully to server.");
+            } catch (MongoException me) {
+                System.err.println("An error occurred while attempting to run a command: " + me);
+            }
+        }
+    }
+}
