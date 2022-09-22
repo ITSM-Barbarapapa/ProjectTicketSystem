7eb48a9091fec2239cae061098f0c8c3053458de
l-huisman <64755533+l-huisman@users.noreply.github.com>
2022-09-22 15:25:28 +0200

    Made the BaseDAO with Connect to Database method

--- DAL/src/main/java/com/ProjectTicketSystemDal/BaseDAO.java
+++ DAL/src/main/java/com/ProjectTicketSystemDal/BaseDAO.java
@@ -1,32 +1,31 @@
 package com.ProjectTicketSystemDal;
 
-
-import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
-import org.bson.BsonDocument;
-import org.bson.BsonInt64;
-import org.bson.Document;
-import org.bson.conversions.Bson;
-
-
 
 public class BaseDAO
 {
-    private final String uri = "mongodb+srv://user:user@users.jbnxxyk.mongodb.net/test ";
+    private final String uri = "mongodb+srv://user:user@users.jbnxxyk.mongodb.net/test";
+    static void main(String[] args)
+    {
+        System.out.println("Main method is empty!");
+    }
 
-
-    protected void ConnectDatabase() {
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("admin");
-            try {
-                Bson command = new BsonDocument("ping", new BsonInt64(1));
-                Document commandResult = database.runCommand(command);
-                System.out.println("Connected successfully to server.");
-            } catch (MongoException me) {
-                System.err.println("An error occurred while attempting to run a command: " + me);
-            }
+    protected MongoDatabase ConnectDatabase()
+    {
+        try {
+            MongoClient mongoClient = MongoClients.create(uri);
+            MongoDatabase database = mongoClient.getDatabase("ProjectTicketSystem");
+            return database;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
         }
+    }
+
+    protected void CloseDatabase()
+    {
+        // Add Code Here...
     }
 }
