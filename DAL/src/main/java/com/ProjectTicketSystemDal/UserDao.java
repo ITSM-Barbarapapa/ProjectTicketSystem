619e48af996007b7026c44b1166b3eb075f381c3
l-huisman <64755533+l-huisman@users.noreply.github.com>
2022-09-22 16:17:58 +0200

    UserDAO added

--- DAL/src/main/java/com/ProjectTicketSystemDal/UserDao.java
+++ DAL/src/main/java/com/ProjectTicketSystemDal/UserDao.java
@@ -1,18 +1,58 @@
 package com.ProjectTicketSystemDal;
 
-import com.mongodb.MongoException;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-public class UserDao extends BaseDAO{
+public class UserDAO
+{
+    MongoDatabase database;
 
-    public UserDao() {
-        BaseDAO baseDAO = new BaseDAO();
-        MongoDatabase database = baseDAO.ConnectDatabase();
-        MongoCollection<Document> collection = database.getCollection("users");
+    public UserDAO()
+    {
+        database = new BaseDAO().ConnectDatabase();
+    }
+
+    public static void main(String[] args)
+    {
+        UserDAO userDAO = new UserDAO();
+        System.out.println("Adding user to database");
+        userDAO.AddTestUser(new User("6", "Luke","Luke123!", Role.Employee));
+        userDAO.GetUser("6");
+    }
+
+    private MongoCollection GetCollection()
+    {
+        try {
+            return database.getCollection("Users");
+        } catch (Exception e) {
+            System.out.println("An error occurred when getting the collection" + e.getMessage());
+            return null;
+        }
+    }
+
+    private User GetUser(String userID)
+    {
+        Document found = (Document) GetCollection().find(new Document("UserID", userID)).first();
+        if (found == null)
+        {
+            System.out.println("User not found");
+            return null;
+        }
+        System.out.println("User found");
+        System.out.println(found.toJson());
+        return new User(found.getString("UserID"),
+                found.getString("Username"),
+                found.getString("Password"));
+    }
+
+    private void AddTestUser(User user)
+    {
+        Document document = new Document("UserID", user.UserID)
+                .append("Username", user.Username)
+                .append("Password", user.Password)
+                .append("Role", user.Role);
+        GetCollection().insertOne(document);
+        System.out.println("User added");
     }
 }
-
