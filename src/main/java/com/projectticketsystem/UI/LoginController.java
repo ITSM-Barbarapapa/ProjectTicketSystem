215c7a491b74cae37eabac709c3564e438f41b26
TimV22 <Timv22.tim@gmail.com>
2022-09-29 15:36:12 +0200

    added login-view.fxml (Done)

--- src/main/java/com/projectticketsystem/UI/LoginController.java
+++ src/main/java/com/projectticketsystem/UI/LoginController.java
@@ -1,4 +1,26 @@
 package com.projectticketsystem.UI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class LoginController {
+    @FXML
+    TextField usernameField;
+
+    @FXML
+    VBox vBoxLogin;
+
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
+    private void onLoginButtonClicked() {
+        System.out.println("Username: " + usernameField.getText());
+        System.out.println("Password: " + passwordField.getText());
+    }
+
+
+
 }
