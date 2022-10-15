dd9970bcffb329aa42e5a0e262510d50948a01aa
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2022-10-15 20:02:55 +0200

    login

--- src/main/java/com/projectticketsystem/UI/StartApplication.java
+++ src/main/java/com/projectticketsystem/UI/StartApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("ticket-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("login-view.fxml"));
         TicketDAO ticketDAO = new TicketDAO();
 
 
