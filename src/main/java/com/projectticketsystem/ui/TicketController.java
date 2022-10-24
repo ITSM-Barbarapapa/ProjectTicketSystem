43977f5aa61789f277d82622b7ac74f5f740a7d6
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2022-10-24 13:48:49 +0200

    push

--- src/main/java/com/projectticketsystem/ui/TicketController.java
+++ src/main/java/com/projectticketsystem/ui/TicketController.java
@@ -199,7 +199,7 @@ public class TicketController extends BaseController implements Initializable {
     @FXML
     public void onMyTicketIconClick(MouseEvent mouseEvent) {
         //TODO Add right controller and view for my tickets
-        loadNextStage("my-tickets-view.fxml", new MyTicketController(user), mouseEvent);
+        loadNextStage("myTickets-view.fxml", new MyTicketController(user), mouseEvent);
         mouseEvent.consume();
     }
 
