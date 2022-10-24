77d3e49d27c4b98e182257911e5822e088335612
TimV22 <Timv22.tim@gmail.com>
2022-10-24 13:43:23 +0200

    Start back to login

--- src/main/java/com/projectticketsystem/ui/StartApplication.java
+++ src/main/java/com/projectticketsystem/ui/StartApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("ticket-list-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Green Desk");
         stage.setResizable(false);
